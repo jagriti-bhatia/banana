ac04f8477195eabcae633ad8341a254b5fd597c4
jagriti-bhatia <73152088+jagriti-bhatia@users.noreply.github.com>
2021-02-27 15:57:02 +0530

    Update banana1.cpp

--- banana1.cpp
+++ banana1.cpp
@@ -1,16 +1,55 @@
-#include<fstream>
 #include<iostream>
 #include<stdio.h>
 #include<string.h>
 #include<string>
-#include <stdlib>
+#include <stdlib.h>
 #include<iomanip>
 #include<process.h>
+#include<fstream>
 
-class exam(){
+using namespace std;
+
+class exam
+{
 
 };//end of exam
 
-void main(){
-    cout<<"Hello world";
-}//end of main
+void readfile()
+{
+    //function to read the file
+}
+
+void writefile()
+{
+    for(int i=0; i<n_questions; i++)
+    {
+
+    }
+    //function to write into the file
+}
+
+void create_question_paper()
+{
+    //to create question paper
+}
+
+int main()
+{
+    cout<<"Hello World!";
+
+    fstream fin;
+    fin.open("Physics.txt", ios::in);
+    //read file here
+    fin.close();
+
+    fstream fin;
+    fin.open("Chemistry.txt", ios::in);
+    //read file here
+    fin.close();
+
+    fstream fin;
+    fin.open("Biology.txt", ios::in);
+    //read file here
+    fin.close();
+}
+
