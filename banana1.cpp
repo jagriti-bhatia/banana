73132250a355b947281990f225f9a8a6bcebef2f
p0709 <53041933+p0709@users.noreply.github.com>
2021-02-27 21:10:12 +0530

    Update banana1.cpp

--- banana1.cpp
+++ banana1.cpp
@@ -7,31 +7,58 @@
 #include<process.h>
 #include<fstream>
 
-//hello
 using namespace std;
 
-class exam
+class exam{
+    int n_questions;
+    char subject[30];
+    int intermediate;
+    int easy;
+    void writefile();
+    public:
+    void question_pattern();
+    void no_of_questions_user();
+    void easy_phy_readfile();
+    void easy_chem_readfile();
+    void easy_bio_readfile();
+    void create_question_paper();
+    void no_of_questions();
+}ques;//end of exam
+
+void exam::no_of_questions(){
+    cout<<"Enter subject: ";
+    cin>>subject;
+    //cout<<"How many questions do you want in the respective subject? ";
+    //cin>>n_questions;
+    int i;
+    for(i=0; i<strlen(subject); i++)
+        subject[i] = tolower(subject[i]);
+}
+
+void exam::question_pattern(){
+    cout<<"Enter number of intermediate questions: ";
+    cin>>intermediate;
+    cout<<"Enter number of easy questions: ";
+    cin>>easy;
+    n_questions = easy + intermediate;
+}//end of fn
+
+void exam::no_of_questions_user()
 {
-
-};//end of exam
-
-int n_questions;
-
-void no_of_questions()
-{
+    int n_questions_user;
     cout<<"How many questions do you want to enter? ";
-    cin>>n_questions;
+    cin>>n_questions_user;
 }
 //cout<<"How many questions do you want to enter";
 //cin>>n_questions;
 
-void readfile()
+void exam::easy_phy_readfile()
 {
     cout<<"Displaying the contents of the file \n";
-
+    cout<<"Easy\n";
     fstream fin("Physics.txt", ios::in);
     char question[500], ch;
-    for(int i=0;  i<n_questions; i++)
+    for(int i=0;  i<easy; i++)
     {
         cout<<"question "<<(i+1)<<": ";
         fin.get(question, 500);
@@ -39,6 +66,17 @@ void readfile()
         fin.get(ch);
     }
     fin.close();
+    cout<<"Intermediate\n";
+    fstream fin1("physics_intermediate.txt", ios::in);
+    char question1[500], ch1;
+    for(int i=0;  i<intermediate; i++)
+    {
+        cout<<"question "<<(i+1)<<": ";
+        fin1.get(question1, 500);
+        cout<<question1<<"\n";
+        fin1.get(ch1);
+    }
+    fin1.close();
     /*
     while(fin.getline(s, 300)){
         char ch[100];
@@ -49,7 +87,76 @@ void readfile()
     */
 }
 
-void writefile()
+void exam::easy_chem_readfile(){
+    cout<<"Displaying the contents of the file \n";
+    cout<<"Easy\n";
+    fstream fin("Chemistry.txt", ios::in);
+    char question[500], ch;
+    for(int i=0;  i<easy; i++)
+    {
+        cout<<"question "<<(i+1)<<": ";
+        fin.get(question, 500);
+        cout<<question<<"\n";
+        fin.get(ch);
+    }
+    fin.close();
+    cout<<"Intermediate\n";
+    fstream fin1("Chemistry_intermediate.txt", ios::in);
+    char question1[500], ch1;
+    for(int i=0;  i<intermediate; i++)
+    {
+        cout<<"question "<<(i+1)<<": ";
+        fin1.get(question1, 500);
+        cout<<question1<<"\n";
+        fin1.get(ch1);
+    }
+    fin1.close();
+    /*
+    while(fin.getline(s, 300)){
+        char ch[100];
+        fin>>ch;
+        cout<<ch;
+    }//end of while
+    fin.close();
+    */
+}//end of fn chem_readfile
+
+void exam::easy_bio_readfile(){
+    cout<<"Displaying the contents of the file \n";
+    cout<<"Easy\n";
+    fstream fin("Biology.txt", ios::in);
+    char question[500], ch;
+    for(int i=0;  i<easy; i++)
+    {
+        cout<<"question "<<(i+1)<<": ";
+        fin.get(question, 500);
+        cout<<question<<"\n";
+        fin.get(ch);
+    }
+    fin.close();
+    cout<<"Intermediate\n";
+    fstream fin1("Biology_intermediate.txt", ios::in);
+    char question1[500], ch1;
+    for(int i=0;  i<intermediate; i++)
+    {
+        cout<<"question "<<(i+1)<<": ";
+        fin1.get(question1, 500);
+        cout<<question1<<"\n";
+        fin1.get(ch1);
+    }
+    fin1.close();
+    /*
+    while(fin.getline(s, 300)){
+        char ch[100];
+        fin>>ch;
+        cout<<ch;
+    }//end of while
+    fin.close();
+    */
+}//end of fn bio_readfile
+
+
+void exam::writefile()
 {
     cout<<"Accepting data to write into the file \n";
 
@@ -69,26 +176,26 @@ void writefile()
     //function to write into the file
 }
 
-void create_question_paper()
-{
-    //to create question paper
+void exam::create_question_paper(){
+    question_pattern();
+    if(strcmp("physics", subject) == 0){
+        //cout<<"PHY";
+        easy_phy_readfile();
+    }
+    else if(strcmp("chemistry", subject)== 0){
+        cout<<"Chem";
+        easy_chem_readfile();
+    }
+    else if(strcmp("biology", subject)== 0){
+        cout<<"Bio";
+        easy_bio_readfile();
+    }
+    else{
+        cout<<"Invalid input";
+    }
 }
 int main()
 {
-    //cout<<"Hello World!";
-    no_of_questions();
-    writefile();
-    readfile();
-    /*fstream fin;
-    fin.open("Physics.txt", ios::in);
-    //read file here
-    fin.close();
-    fstream fin;
-    fin.open("Chemistry.txt", ios::in);
-    //read file here
-    fin.close();
-    fstream fin;
-    fin.open("Biology.txt", ios::in);
-    //read file here
-    fin.close();*/
+    ques.no_of_questions();
+    ques.create_question_paper();
 }
