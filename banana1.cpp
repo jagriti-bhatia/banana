e5b542bb5428195eb1f392b4c754f0d55fd054b1
p0709 <53041933+p0709@users.noreply.github.com>
2021-02-27 15:29:25 +0530

    Add files via upload

---
+++ banana1.cpp
@@ -0,0 +1,16 @@
+#include<fstream>
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<string>
+#include <stdlib>
+#include<iomanip>
+#include<process.h>
+
+class exam(){
+
+};//end of exam
+
+void main(){
+    cout<<"Hello world";
+}//end of main
